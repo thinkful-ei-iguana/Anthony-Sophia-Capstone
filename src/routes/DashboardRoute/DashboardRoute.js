c8dbabad5e5c4a5dca60f6d19ca55730725bfe95
Sophia Koeut <47515086+cantuus@users.noreply.github.com>
2020-02-03 07:11:43 -0800

    made some comments

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -5,6 +5,12 @@ class DashboardRoute extends Component {
     return (
       <section>
         implement and style me
+        <ul>
+          <li>Show name of Language as a subtitle</li>
+          <li>Sub sub heading for 'Words to Practice'</li>
+          <li>The list of words to practice WITH the count for incorrect and correct on each word</li>
+          <li>Lastly a button to start the practice</li>
+        </ul>
       </section>
     );
   }
--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -5,6 +5,13 @@ class LearningRoute extends Component {
     return (
       <section>
         implement and style me
+        <ul>
+        <li>For each word, it displays the incorrect and correct count X times</li>
+          <li>A total score for how many times it was answered correctly</li>
+          <li>Subheading that says 'Translate the word'</li>
+          <li>Below an input to put your answer</li>
+          <li>Button to submit answer</li>
+        </ul>
       </section>
     );
   }
