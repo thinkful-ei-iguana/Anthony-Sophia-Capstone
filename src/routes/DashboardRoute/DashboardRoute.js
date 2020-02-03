539f9b4c0831a0e205860b7f8886cbe2d04961e1
Sophia Koeut <47515086+cantuus@users.noreply.github.com>
2020-02-03 12:14:11 -0800

    styled button, added dashboard component with minimal styling

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react'
+import Dashboard from '../../components/Dashboard/DashBoard'
 
 class DashboardRoute extends Component {
   render() {
     return (
-      <section>
-        implement and style me
-        <ul>
-          <li>Show name of Language as a subtitle</li>
-          <li>Sub sub heading for 'Words to Practice'</li>
-          <li>The list of words to practice WITH the count for incorrect and correct on each word</li>
-          <li>Lastly a button to start the practice</li>
-        </ul>
-      </section>
+      <Dashboard />
     );
   }
 }
